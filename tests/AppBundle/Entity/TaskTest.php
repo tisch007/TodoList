db2d49cae59f615a50837f956ddfeb388a357516
tisch007 <cyrille.tischenbach@gmail.com>
2018-03-28 14:11:06 +0200

    'Authentication'

---
+++ tests/AppBundle/Entity/TaskTest.php
@@ -0,0 +1,55 @@
+<?php
+namespace Tests\AppBundle\Entity;
+
+use AppBundle\Entity\Task;
+use PHPUnit\Framework\TestCase;
+
+class TaskTest extends TestCase
+{
+    public function testTitle()
+    {
+        $task = new Task;
+        $task->setTitle('testTitle');
+
+        $this->assertEquals('testTitle', $task->getTitle());
+    }
+
+    public function testContent()
+    {
+        $task = new Task;
+        $task->setContent('testContent');
+
+        $this->assertEquals('testContent', $task->getContent());
+    }
+
+    public function testAuthor()
+    {
+        $task = new Task;
+        $task->setAuthor('testAuthor');
+
+        $this->assertEquals('testAuthor', $task->getAuthor());
+    }
+
+    public function testIsDone()
+    {
+        $task = new Task;
+        $task->setIsDone(true);
+
+        $this->assertEquals(true, $task->getIsDone());
+    }
+
+    public function testToggle()
+    {
+        $task = new Task;
+        $this->assertEquals(false, $task->toggle(false));
+    }
+
+    public function testCreatedAt()
+    {
+        $task = new Task;
+        $createdAt = new \Datetime();
+        $task->setCreatedAt($createdAt);
+
+        $this->assertEquals($createdAt, $task->getCreatedAt());
+    }
+}
---
+++ tests/AppBundle/Entity/UserTest.php
@@ -0,0 +1,46 @@
+<?php
+namespace Tests\AppBundle\Entity;
+
+use AppBundle\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testUsername()
+    {
+        $user = new User;
+        $user->setUsername('testUsername');
+
+        $this->assertEquals('testUsername', $user->getUsername());
+    }
+
+    public function testEmail()
+    {
+        $user = new User;
+        $user->setEmail('test@gmail.com');
+
+        $this->assertEquals('test@gmail.com', $user->getEmail());
+    }
+
+    public function testRoles()
+    {
+        $user = new User;
+        $user->setRoles(['ROLE_USER']);
+
+        $this->assertEquals(['ROLE_USER'], $user->getRoles());
+    }
+
+    public function testPassword()
+    {
+        $user = new User;
+        $user->setPassword('AZERTY');
+
+        $this->assertEquals('AZERTY', $user->getPassword());
+    }
+
+    public function testSalt()
+    {
+        $user = new User;
+        $this->assertEquals(NULL, $user->getSalt());
+    }
+}
